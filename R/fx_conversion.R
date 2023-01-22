5062480001e00254be28a65b82c642f8dbc077c6
missarah <sarah_s96@hotmail.com>
2023-01-22 09:58:26 -0800

    updated fx_conversion comments
    updated fx_conversion comments

--- R/fx_conversion.R
+++ R/fx_conversion.R
@@ -1,7 +1,7 @@
 #' Converts a specific amount of money from current currency (curr1) to desired currency (curr2)
 #'
 #' @param curr1 A character vector which is the ticker of the current currency such as 'EUR'
-#' @param curr2 A character vector that is the ticker of the desired currency to convert to such as 'USD'
+#' @param curr2 A character vector which is the ticker of the desired currency to convert to such as 'USD'
 #' @param amt A double vector that is the amount of money to be converted
 #'
 #' @return A double vector. Amount of value converted from currency 1 to currency 2
