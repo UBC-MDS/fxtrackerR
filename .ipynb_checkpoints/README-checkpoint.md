d9e62c463daf326133682b2aca9cd45e50769237
missarah <sarah_s96@hotmail.com>
2023-01-31 23:28:05 -0800

    minor edits

--- .ipynb_checkpoints/README-checkpoint.md
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -4,9 +4,6 @@
 # fxtrackerR
 
 <!-- badges: start -->
-
-[![R-CMD-check](https://github.com/UBC-MDS/fxtrackerR/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/UBC-MDS/fxtrackerR/actions/workflows/R-CMD-check.yaml)
-
 <!-- badges: end -->
 
 This is a R package created as a group project for DSCI_524
@@ -18,7 +15,7 @@ historical data as well plotting exchange rate history and profit/loss
 percentage history by specifying a currency pair (and other input
 parameters).
 
-## Functions Description
+## Functions
 
 - `fx_conversion` Convert the input amount of currency 1 to currency 2
   based on the latest available exchange rate.
@@ -42,7 +39,7 @@ You can install the development version of fxtrackerR from
 [GitHub](https://github.com/) with:
 
 ``` r
-install.packages("devtools")
+# install.packages("devtools")
 devtools::install_github("UBC-MDS/fxtrackerR")
 ```
 
@@ -52,62 +49,5 @@ This is a basic example which shows you how to solve a common problem:
 
 ``` r
 library(fxtrackerR)
+## basic example code
 ```
-
-### To convert a specific amount of money from current currency (curr1) to desired currency (curr2):
-
-    fx_conversion('EUR', 'USD', 150.75)
-
-163.68
-
-### To look up the first date (reverse chronological order) on which the target price falling between day high and day low based on the availability of data:
-
-    fx_rate_lookup('EURUSD', 1.072)
-
-‘2023-01-10’
-
-### To visualize the trend of the exchange rate of a currency pair between the selected start date and end date:
-
-    price_trend_viz('EURUSD', '2018-12-01', '2022-12-01', 'high')
-
-![](man/figures/price_trend_viz.png)
-
-### To visualize the trend of the profit and loss of a currency pair between the selected start date and end date:
-
-**If a line chart is specified in the input:**
-
-    pl_trend_viz("EURUSD", "2020-01-01", "2022-01-01", 'line')  
-
-![](man/figures/pl_trend_viz_line.png)
-
-**If an area chart is specified in the input:**
-
-    pl_trend_viz("EURUSD", "2020-01-01", "2022-01-01", 'area')
-
-![](man/figures/pl_trend_viz_area.png)
-
-## Documentation
-
-A more detailed information about our package can found seen on vignette
-[here]()
-
-## Contributors
-
-- Sarah Abdelazim
-- Markus Nam
-- Crystal Geng
-- Lennon Au-Yeung
-
-## Dependencies
-
-- dplyr == 1.1.0
-- ggplot2 == 3.4.0
-- tidyquant == 1.0.6
-- rlang == 1.0.6
-- testthat (\>= 3.0.0)
-
-## License
-
-`fxtracker` was created by Sarah Abdelazim, Markus Nam, Crystal Geng and
-Lennon Au-Yeung. It is licensed under the terms of the MIT license.
-<!-- #endregion -->
---
+++ tests/testthat/.ipynb_checkpoints/test-fxtrackerR-checkpoint.R
@@ -0,0 +1,120 @@
+# test for fx_conversion
+
+# check input type of curr1
+test_that("Check input type of curr1", {
+  expect_error(fx_conversion(1, 'USD', 100), "The first parameter must be a string!")
+})
+
+# check input type of curr2
+test_that("Check input type of curr2", {
+  expect_error(fx_conversion('USD', 1, 100), "The second parameter must be a string!")
+})
+
+# check input type of amt
+test_that("Check input type of amt", {
+  expect_error(fx_conversion('USD', 'EUR', "X"), "The third parameter must be numeric!")
+})
+
+# check output type is numeric
+test_that("Check if output is numeric", {
+  expect_true(is.numeric(fx_conversion('EUR', 'USD', 100)))
+})
+
+# check if all arguments are passed
+test_that("Check if all arguments are passed", {
+    expect_error(fx_conversion('USD', 'EUR'), 'argument "amt" is missing, with no default')
+})
+
+# check if correct arguments are passed
+test_that("Check if correct arguments are passed", {
+    expect_error(fx_conversion('USD', 'EUR', a), "object 'a' not found")
+})
+
+# check for invalid ticker
+test_that("Check for invalid ticker", {
+  expect_error(fx_conversion('USD', 'XXX', 100), "You have entered an invalid foreign ticker! Try again.")
+})
+
+# test for fx_rate_lookup
+
+test_that("The first parameter (curr) should be a string.", {
+  expect_error(fx_rate_lookup(123, 0.09))
+})
+
+test_that("The second parameter (target_px) should be numeric.", {
+  expect_error(fx_rate_lookup('JPYHKD', '0.09'))
+})
+
+test_that("The first parameter (curr) should be a valid ticker.", {
+  expect_error(fx_rate_lookup('ABC', 0.09))
+})
+
+test_that("Return string should be in the format YYYY-MM-DD.", {
+  expect_match(fx_rate_lookup('JPYHKD', 0.09), '^\\d{4}-\\d{2}-\\d{2}$')
+})
+
+
+# test for pl_trend_viz
+
+#test currency format
+test_that("Error of currency format",{expect_error(pl_trend_viz(123,' 2020-12-31', '2021-12-31','area'))})
+
+#test start_date type
+test_that("Error of start_date format",{expect_error(pl_trend_viz('EURUSD',123, '2021-12-31','area'))})
+
+#test end_date type
+test_that("Error of end_date format",{expect_error(pl_trend_viz('EURUSD','2020-12-31', 123,'area'))})
+
+#test start_date > end_date
+test_that("Error of end_date format",{expect_error(pl_trend_viz('EURUSD','2022-12-31', '2020-12-31','area'))})
+
+#test chart_type error
+test_that("Error of end_date format",{expect_error(pl_trend_viz('EURUSD','2020-12-31', '2022-12-31','bar'))})
+
+#test invalid ticker error
+test_that("Error of end_date format",{expect_error(pl_trend_viz('ABC','2020-12-31', '2022-12-31','bar'))})
+
+#test for chart mapping
+chart <- pl_trend_viz("EURUSD",'2020-12-31', '2021-12-31','line')
+test_that('Plot should map Date to x-axis and map pct_change to y-axis ', {
+  expect_true("date" == rlang::get_expr(chart$mapping$x))
+  expect_true("pct_change" == rlang::get_expr(chart$mapping$y))})
+
+
+# test for price_trend_viz
+
+test_that("Check input type of curr", {
+  expect_error(price_trend_viz(1, '2012-02-01', '2014-12-31', 'high'), "curr needs to be of character type.")
+})
+
+test_that("Check input type of start_date", {
+  expect_error(price_trend_viz('EURUSD', 2.5, '2014-12-31', 'low'), "start_date needs to be of character type.")
+})
+
+test_that("Check input type of end_date", {
+  expect_error(price_trend_viz('EURUSD', '2012-02-01', 333, 'close'), "end_date needs to be of character type.")
+})
+
+test_that("Check option of plotting is from 'open', 'high', 'low' or 'close'", {
+  expect_error(price_trend_viz('EURUSD', '2012-12-31', '2014-12-31', 'highest'), 
+               "Your option of plotting should be from 'open', 'high', 'low' or 'close'")
+})
+
+test_that("Check end date entered is later than 2003-12-01", {
+  expect_error(price_trend_viz('EURUSD', '1992-02-01', '1994-12-31', 'high'), 
+               "No data exists before 2003-12-01, please try again.")
+})
+
+test_that("Check start date entered is earlier than or equal to today", {
+  expect_error(price_trend_viz('EURUSD', '2023-12-31', '2024-12-31', 'high'), 
+               "You entered a start date later than today, please try again.")
+})
+
+test_that("Check if the same dates are entered for start date and end date", {
+  expect_error(price_trend_viz('EURUSD', '2013-12-31', '2013-12-31', 'high'), 
+               "Trend of one day cannot be plotted, please enter duration longer than one day")
+})
+
+test_that("Check if output is a ggplot object", {
+  expect_true(ggplot2::is.ggplot(price_trend_viz('EURUSD', '2012-02-01', '2014-12-31', 'high')))
+})
